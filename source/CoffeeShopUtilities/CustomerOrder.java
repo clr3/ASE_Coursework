5594cf7db334b4dc7c31e4d8b2ec9aa9217ced2c
Sethu Lekshmy <sethulekshmy@sethus-mbp.home>
2019-02-15 00:34:17 +0000

    OrderManager class and order history csv added. Place holder for CustomerOrder added

---
+++ source/CoffeeShopUtilities/CustomerOrder.java
@@ -0,0 +1,16 @@
+package CoffeeShopUtilities;
+
+import java.util.ArrayList;
+import java.util.Date;
+
+public class CustomerOrder {
+	
+	private String customerId;
+	private String orderId;
+	private ArrayList<FoodItem> orderItems;
+	private Date timestamp;
+	
+	//TODO
+	//Implement the methods defined in class diagram
+
+}
