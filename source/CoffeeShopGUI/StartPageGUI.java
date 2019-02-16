cb7697add8482f4e7065e228396b58cd7f1804cf
Cristina Rivera <clr3@hw.ac.uk>
2019-02-16 15:32:50 +0000

    Main GUI

--- source/CoffeeShopGUI/StartPageGUI.java
+++ source/CoffeeShopGUI/StartPageGUI.java
@@ -1,43 +1,63 @@
 package CoffeeShopGUI;
+import java.awt.*;
+import javax.swing.*;
+
+
 /**
  * @Author Cristina Rivera 	<clr3@hw.ac.uk>
  * 
+ * JPanel which provides access to: 
+ *  				menu (for customers)
+ * 					staff view 
  * */
-import javax.swing.*;
 
-public class StartPageGUI {
+public class StartPageGUI extends JPanel{
 	
-	private int mainFrameHight = 400;
-	private int mainFrameWidth = 300;
+	private GridBagConstraints constraints = new GridBagConstraints();
+	
+	private JButton staffButton;
+	private JButton customerButton;
+
 	
 	
-	public StartPageGUI() {}
+	/**Initialise the buttons */
+	public StartPageGUI() {
+		setLayout(new GridBagLayout());
+		staffButton = new JButton("Staff");
+		customerButton = new JButton("View Menu");
+		
+		
+		
+		int x, y;
+		constraints.gridheight= 3;  //Span 3 rows
+		constraints.gridwidth = 5;  //Span 5 columns
+		constraints.weightx = 1.0;
+        constraints.weighty = 1.0;
+        constraints.fill = GridBagConstraints.BOTH;
+        constraints.anchor = GridBagConstraints.BELOW_BASELINE;
+       
+		add_component(customerButton, x = 0, y = 0);
+		
+		constraints.weightx = 0.5;
+        constraints.weighty = 0.5;
+        constraints.fill = GridBagConstraints.EAST;
+		constraints.gridheight = 1; //set back
+		constraints.gridwidth = 1;
+		add_component(staffButton, x = 3, y = 6);
+				
+				
+	}
 	
 	/**
-	 * Frame which provides access to: 
-	 * 				menu (for customers)
-	 * 				staff view 
-	 * 
-	 * |---------------|
-	 * |			   |
-	 * |    Staff	   |
-	 * |   Customer    |
-	 * |---------------|
+	 * As seen in this tutorial: 
+	 * https://www.oreilly.com/library/view/learning-java-4th/9781449372477/ch19s06.html#learnjava3-CHP-19-FIG-7
 	 * */
-	public void show_start_page() {
-		JFrame frame = new JFrame("Coffee Shop Start");		
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		
-	
-		frame.setSize(mainFrameWidth , mainFrameHight);
-			
-		
-		JButton staffButton = new JButton("Staff");
-		JButton customerButton = new JButton("Customer");
-		
-		frame.getContentPane().add(customerButton);
-		frame.getContentPane().add(staffButton);
-		frame.setVisible(true);
+	public void add_component(Component component,int x , int y) {
+		constraints.gridx = x;
+		constraints.gridy = y;
+		add(component, constraints);
 
 	}
+	
+	
 }
